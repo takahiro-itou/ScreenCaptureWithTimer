1c53ad0ad3dc0d9833f01376e47f78e83a92f5d9
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-10-31 21:22:35 +0900

    API の宣言を追加

---
+++ View/WinAPI.vb
@@ -0,0 +1,30 @@
+﻿
+Imports System.Runtime.InteropServices
+
+Module WinAPI
+
+Public Const SRCCOPY As Integer = &HCC0020
+
+<DllImport("gdi32.dll")> _
+Public Function BitBlt(
+    ByVal hDestDC As IntPtr,
+    ByVal X As Integer,
+    ByVal Y As Integer,
+    ByVal nWidth As Integer,
+    ByVal nHeight As Integer,
+    ByVal hSrcDC As IntPtr,
+    ByVal xSrc As Integer,
+    ByVal ySrc As Integer,
+    ByVal dwRop As Integer) As Integer
+End Function
+
+<DllImport("user32.dll")> _
+Public Functioni GetDC(ByVal hWnd As IntPtr) As IntPtr
+End Function
+
+<DllImport("user32.dll")> _
+Public Functioni ReleasetDC(
+        ByVal hWnd As IntPtr, ByVal hDC As IntPtr) As IntPtr
+End Function
+
+End Module
